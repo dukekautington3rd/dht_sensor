a7c97ba974a4b7a063543f11faf4862fe7df3233
Lon Kaut <lonkaut@gmail.com>
2022-11-20 23:17:13 -0600

    change to F instead of C

--- shtc3_sensor.py
+++ shtc3_sensor.py
@@ -12,7 +12,8 @@ sht = adafruit_shtc3.SHTC3(i2c)
 
 while True:
     temperature, relative_humidity = sht.measurements
-    print("Temperature: %0.1f C" % temperature)
+    print(time.ctime())
+    print("Temperature: %0.1f F" % temperature * 1.8 + 32)
     print("Humidity: %0.1f %%" % relative_humidity)
     print("")
-    time.sleep(1)
+    time.sleep(60)
